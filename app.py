7be878660c6d26b30d70b3ccedbbd106709ea131
marcpsc57 <90506430+marcpsc57@users.noreply.github.com>
2026-03-26 19:50:58 -0400

    app.py

--- app.py
+++ app.py
@@ -5,11 +5,41 @@ from google.oauth2.service_account import Credentials
 
 st.set_page_config(page_title="BRAINROT COLLECTOR", layout="wide")
 
+# ON COPIE LA CLÉ SUR PLUSIEURS LIGNES ICI
+PRIVATE_KEY = """-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDTxx+vkltYKShm
+ou26Gnfx6o68qjPyvBSpjbiuFxReayb4hxvnOv18R3JSPQuzhUxd9q985UgJ6jbU
+VGjjIWrut74nhXVw49q5OnPji58Ukage9GD/0l66IRGb56UdvRk1sOOYSeAr+ky1
+D6CSiNK3FlW4HgcJq0sud3wp8IN8I62wp0RFtxR53kLbzJzToRjhA+dqFVqxC5dD
+1QoAcC4P3kWVNjFpzbYPSUtYboBmoquhT1i6Ntglb9NeRMBH6zG3Y4cUUoAtytJv
+5omnw3v0Vyfsfic73m8Vgz0ujHCxuxvnl47gjOIA6Fp4ew7RR1zr+RI1yU9hnaJH
+NVoIJr5BAgMBAAECggEABP/IH6iBFkB4zbfGduxkHN5xoo0TLJrUEzQPR4cah0eO
+LBNMEp4TKNrAErunl0QRZfhE/c9k7ydQ8RvHKDxsK34uzjycOvnapQvMO536l31V
+kQR/C9mVvEempHiNg+QTEHrFsUe7fleaYF2h8lo/PMyZdv8OIWRCd4fLyIApqcBG
+sVylqgHWw4xa0Z4HJIrLXhFtVQpPJHz5IiquBbFUrmLE5A1NLDXAulwQdKqys+1d
+nWQCossX6j9YPdTw+0WgDpKfXCd2OAv8Z9aoYG6iYOI0GI0cKhrXX7lxJEO7oRZGZ
+RVudCIV/45EXnk02iL0du1yc+ZEy3lxqcIG07TkZaQKBgQDw1XazQeobx3QF03lA
++Jgyrb6z5mU7ae11F4xsU5zF+DKFfalrk1/wJ5DV6gPD3Yz4VlIkTcZMKxxw93iq
+snxrrneoG8oEVWPBZadWJOveVnQOOvOEnn+j9/qcq7C+NwQ8GwY1dQJrTrdiMU5w
+vk/17pXHumsiylTZgn1g+rgNeQKBgQDhHT3XecWFglE+62Dya9n3iTw2iGpIAUKk
+dot6kRkmBHhRKYxV8hNnvm1QfBr7+aj8E5n2CwNkxQrkJhNnLF5j2eo0a2g3IuIQ
+j8jvv22yICjqDh+8KPcED5dFI3VH/CjaOK9RlDcZRC/EXshdktWtavIQC6fnpWR3
+Oocng9AtCQKBgHCmeMpql5X82sFq5LnGTnLOIHjH2SxMZR0zMaTuC1Pyv7b/S4Md
+QW/1IeNdfftdI3Z58hw3IP054SjaKOA3cslvp1rD/N/ADVRGN1qtFINAjzKk0omD
+FhNdiCVGjfq7g0iWH3Zb0BDflhnhFF7aNk7EFd2BzmFMoRnI1trJC9SJAoGAfHCx
+nst95tOfpCaMMIHZGM3QFeQ/H0K4BrPBrAgIbqMxnxlX1Yb1DxOGbhBZWxFuqck5T
+mZNU+OuDrcLujuYT95aKxRdqsK/zz7vsE7v6Y7ErbNcJ6/WpNvF3aZzERFq8KoWT
+RMFuA+WRkjfjAvccb5Ti4sPOuVrQQqhz9gADoCECgYEA6SGq8yFcj0D/a2WfLgRd
+wv3wVa6KyPwF/76ap2GabwNFp/7a6GHnVHk+quLtPpV6hi7Bfo7m7AINRCiTpQh5
+nu4evFiO9JRM/wOR6oepL67H+jNDvq4Ea9g7t7QPyy/92aGaNYlPYZAqEazIejD6P
+ngo1rYlMIKdhVHrntqYUuxKc=
+-----END PRIVATE KEY-----"""
+
 SERVICE_ACCOUNT_INFO = {
     "type": "service_account",
     "project_id": "brainrot-project-491423",
     "private_key_id": "dd28909d6aaaaf314cc05b83ce8856ff9c9fe03e",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDTxx+vkltYKShm\nou26Gnfx6o68qjPyvBSpjbiuFxReayb4hxvnOv18R3JSPQuzhUxd9q985UgJ6jbU\nVGjjIWrut74nhXVw49q5OnPji58Ukage9GD/0l66IRGb56UdvRk1sOOYSeAr+ky1\nD6CSiNK3FlW4HgcJq0sud3wp8IN8I62wp0RFtxR53kLbzJzToRjhA+dqFVqxC5dD\n1QoAcC4P3kWVNjFpzbYPSUtYboBmoquhT1i6Ntglb9NeRMBH6zG3Y4cUUoAtytJv\n5omnw3v0Vyfsfic73m8Vgz0ujHCxuxvnl47gjOIA6Fp4ew7RR1zr+RI1yU9hnaJH\nNVoIJr5BAgMBAAECggEABP/IH6iBFkB4zbfGduxkHN5xoo0TLJrUEzQPR4cah0eO\nLBNMEp4TKNrAErunl0QRZfhE/c9k7ydQ8RvHKDxsK34uzjycOvnapQvMO536l31V\nkQR/C9mVvEempHiNg+QTEHrFsUe7fleaYF2h8lo/PMyZdv8OIWRCd4fLyIApqcBG\nsVylqgHWw4xa0Z4HJIrLXhFtVQpPJHz5IiquBbFUrmLE5A1NLDXAulwQdKqys+1d\nWQCossX6j9YPdTw+0WgDpKfXCd2OAv8Z9aoYG6iYOI0GI0cKhrXX7lxJEO7oRZGZ\nRVudCIV/45EXnk02iL0du1yc+ZEy3lxqcIG07TkZaQKBgQDw1XazQeobx3QF03lA\n+Jgyrb6z5mU7ae11F4xsU5zF+DKFfalrk1/wJ5DV6gPD3Yz4VlIkTcZMKxxw93iq\nsnxrrneoG8oEVWPBZadWJOveVnQOOvOEnn+j9/qcq7C+NwQ8GwY1dQJrTrdiMU5w\nvk/17pXHumsiylTZgn1g+rgNeQKBgQDhHT3XecWFglE+62Dya9n3iTw2iGpIAUKk\ndot6kRkmBHhRKYxV8hNnvm1QfBr7+aj8E5n2CwNkxQrkJhNnLF5j2eo0a2g3IuIQ\nj8jvv22yICjqDh+8KPcED5dFI3VH/CjaOK9RlDcZRC/EXshdktWtavIQC6fnpWR3\nOocng9AtCQKBgHCmeMpql5X82sFq5LnGTnLOIHjH2SxMZR0zMaTuC1Pyv7b/S4Md\QW/1IeNdfftdI3Z58hw3IP054SjaKOA3cslvp1rD/N/ADVRGN1qtFINAjzKk0omD\FhNdiCVGjfq7g0iWH3Zb0BDflhnhFF7aNk7EFd2BzmFMoRnI1trJC9SJAoGAfHCx\nst95tOfpCaMMIHZGM3QFeQ/H0K4BrPBrAgIbqMxnxlX1Yb1DxOGbhBZWxFuqck5T\nmZNU+OuDrcLujuYT95aKxRdqsK/zz7vsE7v6Y7ErbNcJ6/WpNvF3aZzERFq8KoWT\nRMFuA+WRkjfjAvccb5Ti4sPOuVrQQqhz9gADoCECgYEA6SGq8yFcj0D/a2WfLgRd\wv3wVa6KyPwF/76ap2GabwNFp/7a6GHnVHk+quLtPpV6hi7Bfo7m7AINRCiTpQh5\nu4evFiO9JRM/wOR6oepL67H+jNDvq4Ea9g7t7QPyy/92aGaNYlPYZAqEazIejD6P\ngo1rYlMIKdhVHrntqYUuxKc=\n-----END PRIVATE KEY-----\n",
+    "private_key": PRIVATE_KEY,
     "client_email": "brainrot-bot@brainrot-project-491423.iam.gserviceaccount.com",
     "client_id": "107186528494643778636",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -21,10 +51,7 @@ SERVICE_ACCOUNT_INFO = {
 @st.cache_resource
 def connect_to_gsheet():
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    # On nettoie la clé ici
-    info = SERVICE_ACCOUNT_INFO.copy()
-    info["private_key"] = info["private_key"].replace("\\n", "\n")
-    creds = Credentials.from_service_account_info(info, scopes=scope)
+    creds = Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO, scopes=scope)
     return gspread.authorize(creds)
 
 try:
@@ -44,7 +71,7 @@ if not df.empty:
         with c1:
             is_owned = str(row.get('possede', 0)) == "1"
             if st.button("✅" if is_owned else "⬜", key=f"btn_{index}"):
-                new_val = 0 if is_owned else 1
+                new_val = 1 if not is_owned else 0
                 worksheet.update_cell(index + 2, 5, new_val)
                 st.rerun()
         with c2:
